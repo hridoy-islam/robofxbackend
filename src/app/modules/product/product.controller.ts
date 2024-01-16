e5a37d33766acd02080e6881b621ed70cff2c96a
Shahadat Hossen <shahadat.07.sh@gmail.com>
2024-01-16 13:11:46 +0600

    one changes

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,55 +1,54 @@
-import httpStatus from "http-status";
-import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
-import { ProductServices } from "./product.service";
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { ProductServices } from './product.service';
 
 const createProduct = catchAsync(async (req, res) => {
-    const result = await ProductServices.createProductToDB(req.body)
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Product is created succesfully',
-        data: result,
-    });
-})
+  console.log('here is the res', res);
+  const result = await ProductServices.createProductToDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product is created succesfully',
+    data: result,
+  });
+});
 
 const getAllproduct = catchAsync(async (req, res) => {
-    const result = await ProductServices.getAllProductsFromDB(req.query);
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Product retrived succesfully',
-        data: result,
-    });
-})
+  const result = await ProductServices.getAllProductsFromDB(req.query);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product retrived succesfully',
+    data: result,
+  });
+});
 
 const getSingleProduct = catchAsync(async (req, res) => {
-    const { id } = req.params
-    const result = await ProductServices.getSingleProductFromDB(id);
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Product retrived succesfully',
-        data: result,
-    });
-})
+  const { id } = req.params;
+  const result = await ProductServices.getSingleProductFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product retrived succesfully',
+    data: result,
+  });
+});
 
 const updateProduct = catchAsync(async (req, res) => {
-    const   { id }   = req.params
-    const result = await ProductServices.updateProductIntoDB(id, req.body);
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Product updated succesfully',
-        data: result,
-    });
-})
-
-
+  const { id } = req.params;
+  const result = await ProductServices.updateProductIntoDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product updated succesfully',
+    data: result,
+  });
+});
 
 export const ProductControllers = {
-    createProduct,
-    getAllproduct,
-    getSingleProduct,
-    updateProduct
-}
+  createProduct,
+  getAllproduct,
+  getSingleProduct,
+  updateProduct,
+};
