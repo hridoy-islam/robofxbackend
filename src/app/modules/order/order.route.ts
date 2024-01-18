75f0795be489f237a8edf190cdcc25247397a53d
Shahadat Hossen <shahadat.07.sh@gmail.com>
2024-01-18 20:23:46 +0600

    auth removed

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -2,18 +2,28 @@ import express from 'express';
 import { OrderControllers } from './order.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { createOrderValidationSchema } from './order.validation';
-import auth from '../../middlewares/auth';
+// import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.get('/', auth('admin', 'user'), OrderControllers.getAllOrder);
-router.get('/:id', auth('admin', 'user'), OrderControllers.getSingleOrder);
+router.get('/', OrderControllers.getAllOrder);
+router.get('/:id', OrderControllers.getSingleOrder);
 router.post(
   '/',
-  auth('admin'),
+
   validateRequest(createOrderValidationSchema),
   OrderControllers.createOrder,
 );
-router.patch('/:id', auth('admin'), OrderControllers.updateOrder);
+router.patch('/:id', OrderControllers.updateOrder);
+
+// router.get('/', auth('admin', 'user'), OrderControllers.getAllOrder);
+// router.get('/:id', auth('admin', 'user'), OrderControllers.getSingleOrder);
+// router.post(
+//   '/',
+//   auth('admin'),
+//   validateRequest(createOrderValidationSchema),
+//   OrderControllers.createOrder,
+// );
+// router.patch('/:id', auth('admin'), OrderControllers.updateOrder);
 
 export const OrderRoutes = router;
