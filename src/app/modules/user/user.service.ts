e229dde85de7de915213ca0bd933b25c8cc674cd
Md. Ridoy Islam <md.ridoyislam@Mds-MacBook-Pro.local>
2024-01-18 20:49:26 +0600

    error solved

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,11 @@
-import httpStatus from "http-status";
-import QueryBuilder from "../../builder/QueryBuilder";
-import AppError from "../../errors/AppError";
-import { TUser, TUserWallet } from "./user.interface";
-import { User } from "./user.model";
-import { walletValidationSchema } from "../wallet/wallet.validation";
+import httpStatus from 'http-status';
+import QueryBuilder from '../../builder/QueryBuilder';
+import AppError from '../../errors/AppError';
+import { TUser, TUserWallet } from './user.interface';
+import { User } from './user.model';
 
 const getAllUserFromDB = async (query: Record<string, unknown>) => {
-  const userQuery = new QueryBuilder(
-    User.find(),
-    query,
-  )
+  const userQuery = new QueryBuilder(User.find(), query)
     .filter()
     .sort()
     .paginate()
@@ -22,15 +18,20 @@ const getAllUserFromDB = async (query: Record<string, unknown>) => {
     meta,
     result,
   };
-}
+};
 
 const getSingleUserFromDB = async (id: string) => {
   const result = await User.findById(id);
   return result;
-}
+};
 
 const updateUserIntoDB = async (id: string, payload: Partial<TUser>) => {
-  const { wallets, billing_information, personal_information, contact_information, ...remainingStudentData } = payload;
+  const {
+    billing_information,
+    personal_information,
+    contact_information,
+    ...remainingStudentData
+  } = payload;
   const modifiedUpdatedData: Record<string, unknown> = {
     ...remainingStudentData,
   };
@@ -53,69 +54,58 @@ const updateUserIntoDB = async (id: string, payload: Partial<TUser>) => {
     }
   }
 
-  const result = await User.findByIdAndUpdate(id, modifiedUpdatedData, { new: true, runValidators: true, });
+  const result = await User.findByIdAndUpdate(id, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
 
   return result;
-}
+};
 
 const addNewWallet = async (id: string, payload: TUserWallet) => {
   try {
-    const createdData = await User.findByIdAndUpdate(id,
-      { "$push": { "wallets": payload } },
-      { "new": true, "upsert": true }
+    const createdData = await User.findByIdAndUpdate(
+      id,
+      { $push: { wallets: payload } },
+      { new: true, upsert: true },
     );
     if (createdData) {
       const updatedWallet = await User.findOne({ _id: id });
-      return updatedWallet
-    } else throw new AppError(
-      httpStatus.NOT_FOUND,
-      'Wallet Update Error.',
-    );
+      return updatedWallet;
+    } else throw new AppError(httpStatus.NOT_FOUND, 'Wallet Update Error.');
   } catch (error) {
-    throw new AppError(
-      httpStatus.NOT_FOUND,
-      'New Wallet Create Error.')
+    throw new AppError(httpStatus.NOT_FOUND, 'New Wallet Create Error.');
   }
+};
 
-}
-
-
-
-
-const updateWallet = async (id: string, wallet_id: string, payload: TUserWallet) => {
-
-  try {
-
+const updateWallet = async () =>
+  // id: string,
+  // wallet_id: string,
+  // payload: TUserWallet,
+  {
+    // try {
     // let entries = Object.keys(payload)
     // console.log(typeof payload)
-
     // const updateWalletList = await User.findByIdAndUpdate({
     //   _id: id,
     //   wallets?._id : wallet_id,
-    // }, 
+    // },
     // {
     //   $set: payload
     // });
-
-    if (updateWalletList) {
-      const updatedWallet = await User.findOne({ _id: id });
-      return updatedWallet
-    } else throw new AppError(
-      httpStatus.NOT_FOUND,
-      'Wallet Update Error.',
-    );
-  } catch (error) {
-    throw new AppError(
-      httpStatus.NOT_FOUND,
-      'Wallet Update Error.')
-  }
-
-}
+    //   if (updateWalletList) {
+    //     const updatedWallet = await User.findOne({ _id: id });
+    //     return updatedWallet;
+    //   } else throw new AppError(httpStatus.NOT_FOUND, 'Wallet Update Error.');
+    // } catch (error) {
+    //   throw new AppError(httpStatus.NOT_FOUND, 'Wallet Update Error.');
+    // }
+  };
 
 export const UserServices = {
   getAllUserFromDB,
   getSingleUserFromDB,
   updateUserIntoDB,
   updateWallet,
-  addNewWallet
-}
+  addNewWallet,
+};
